3a5a6923b37c42c8cf2a5802b5c139beed824c55
Marko <msolajic@gmail.com>
2017-09-13 01:46:31 +0200

    Signed-off-by: Marko <msolajic@gmail.com>

--- module_twitter.lua
+++ module_twitter.lua
@@ -42,11 +42,6 @@ function M.run(duration, tweet, fn)
         mipmap = true,
     }
  
-	local imag = resource.load_image{
-        file = tweet.image:copy(),
-        mipmap = true,
-    }
-
     local start_y = 100
 
     local x = 150
@@ -74,7 +69,7 @@ function M.run(duration, tweet, fn)
     end
     a.add(anims.moving_font(S, E, x, y, age .. " ago", 50, 1,1,1,1)); S=S+0.1; y=y+60
 
-    a.add(anims.tweet_profile(S, E, x, start_y, imag, 120))
+    a.add(anims.tweet_profile(S, E, x, start_y, img, 120))
 
     for now in fn.upto_t(E) do
         a.draw(now)
